5dda71735298a4f5f609a7c89f18a6b5f9b13bc1
Mansoor Ali Ibrahim <109726842+mansoor-ali-ibrahim@users.noreply.github.com>
2023-04-14 07:30:11 +0500

    Experiments

--- my-app/app.js
+++ my-app/app.js
@@ -1,12 +1,37 @@
-let person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 50,
-    eyeColor: "blue",
-  };
+// let person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue",
+//   };
 
-  let stringy = JSON.stringify(person);
+//   let stringy = JSON.stringify(person);
 
-  localStorage.setItem("person1", stringy);
+//   localStorage.setItem("person1", stringy);
 
-  console.log(stringy);
+//   console.log(stringy);
+
+// function runForLoop(def){
+//   //for (let i = 0; i <= 10; i++){
+//     return function(rec){
+//       console.log(def+rec);
+//     }
+//   //}
+// }
+// let loopvar = runForLoop(10);
+// loopvar(20);
+//console.log(loopvar);
+
+// let mathVar = Math.random();
+// // let calulateMath = Math.round(mathVar);
+// console.log(mathVar);
+// let calulateMath = Math.floor(mathVar);
+// console.log(calulateMath);
+
+const myBirthday = new Date(2023, 7, 24);
+// console.log(myBirthday.getTime());
+const date = new Date();
+
+let dateDiff = myBirthday.getTime() - date.getTime();
+
+console.log(dateDiff);
